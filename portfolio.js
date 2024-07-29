8381ab64719d9f12919a4b513cf01b5d505448af
Prashant8115 <prashantks2223@gmail.com>
2024-07-29 23:14:27 +0530

    worked on skills page

---
+++ portfolio.js
@@ -0,0 +1,50 @@
+// frontendSkills = [
+//     {
+//         img: '/html.svg',
+//         skillName: "HTML",
+//         age: 20
+//     },
+//     {
+//         img: '/html.svg',
+//         skillName: "CSS"
+//     },
+//     {
+//         img: '/html.svg',
+//         skillName: "Javascript"
+//     }
+//     ,
+//     {
+//         img: '/html.svg',
+//         skillName: "Reactjs"
+//     },
+//     {
+//         img: '/html.svg',
+//         skillName: "APIs"
+//     },
+//     {
+//         img: '/html.svg',
+//         skillName: "Bootstraps"
+//     },
+//     {
+//         img: '/html.svg',
+//         skillName: "Tailwind CSS"
+//     },
+//     {
+//         img: '/html.svg',
+//         skillName: "SASS"
+//     }
+// ]
+
+// let s_container = document.querySelector(".skills-lists")
+// frontendSkills.map((skills) => {
+//     let div = document.createElement('div');
+//     div.classList.add('sd2')
+//     let p = document.createElement('p');
+//     let img = document.createElement('img');
+//     img.setAttribute('src', `${skills.img}`)
+//     img.classList.add('img');
+//     div.appendChild(p);
+//     div.appendChild(img);
+//     p.append(skills.skillName)
+//     s_container.append(div)
+// })
